d6a2ee7c35737eeef974b692788c6f80eec37815
amelie-quintero <224878763+amelie-quintero@users.noreply.github.com>
2026-03-13 15:51:47 -0400

    added method to update tasks and updated return statements

--- server/controllers/TaskController.js
+++ server/controllers/TaskController.js
@@ -8,7 +8,7 @@ exports.getTask = async (req, res) => {
         res.status(200).json({tasks, status: true, msg: "Tasks found successfully."});
     } catch (err) {
         console.error(err);
-        res.status(500).json({status: false, msg: "Internal Server Error"});
+        return res.status(500).json({status: false, msg: "Internal Server Error"});
     }
 }
 exports.getTask = async (req, res) => {
@@ -18,7 +18,7 @@ exports.getTask = async (req, res) => {
         }
         const task = await Task.findById({user: req.user.id, _id: req.params.taskId});
         if (!task) {
-            res.status(400).json({status: false, msg: "No tasks found."});
+            return res.status(400).json({status: false, msg: "No tasks found."});
         }
         res.status(200).json({task, status: true, msg: "Task found successfully."});
     } catch (err) {
@@ -30,10 +30,30 @@ exports.postTask = async (req, res) => {
     try {
         const {description} = req.body;
         if (!description) {
-            return res.status(400).json({status: false, msg: "Description of task not found."});
+            return res.status(400).json({status: false, msg: "Task description not found."});
         }
         const task = await Task.create({user: req.user.id, description});
         res.status(200).json({task, status: true, msg: "Task created successfully."});
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({status: false, msg: "Internal Server Error."});
+    }
+}
+exports.putTask = async (req, res) => {
+    try {
+        const {description} = req.body;
+        if (!description) {
+            return res.status(400).json({status: false, msg: "Task description not found."})
+        }
+        if (!validateObjectId(req.params.taskId)) {
+            return res.status(400).json({status: false, msg: "Invalid task id."});
+        }
+        let task = await Task.findById(req.params.taskId);
+        if (task.user != req.user.id) {
+            return res.status(403).json({status: false, msg: "You can't update the task of another user."});
+        }
+        task = await Task.findByIdAndUpdate(req.params.taskId, {description}, {new: true});
+        res.status(200).json({task, status: true, msg: "Task updated successfully."});
     } catch (err) {
         console.error(err);
         return res.status(500).json({status: false, msg: "Internal Server Error."});
