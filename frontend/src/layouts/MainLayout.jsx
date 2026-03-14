9a1c46cc73b3bd3aa4e745adcfcc1de10fcbcd9a
amelie-quintero <224878763+amelie-quintero@users.noreply.github.com>
2026-03-14 17:34:54 -0400

    added main layout page

---
+++ frontend/src/layouts/MainLayout.jsx
@@ -0,0 +1,14 @@
+import Navbar from '../components/Navbar';
+
+const MainLayout = ({ children }) => {
+    return (
+        <>
+            <div className='relative bg-gray-50 h-screen w-screen overflow-x-hidden'>
+                <Navbar />
+                {children}
+            </div>
+        </>
+    )
+}
+
+export default MainLayout;
